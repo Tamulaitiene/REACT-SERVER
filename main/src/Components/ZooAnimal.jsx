42aa162d0a0ce89079f38061fd0d3266de14c022
Vilija <86228325+Tamulaitiene@users.noreply.github.com>
2021-10-28 13:00:34 +0300

    Crud

--- main/src/Components/ZooAnimal.jsx
+++ main/src/Components/ZooAnimal.jsx
@@ -1,13 +1,18 @@
-function ZooAnimal({animal}) {
+function ZooAnimal({animal, modal}) {
+
+    const  showEdit = () => {
+        modal(animal)
+    }
 
     return (
         <div className="zoo__list__animal">
             <div className="zoo__list__animal__name">{animal.name}</div>
             <div className="zoo__list__animal__type">{animal.type}</div>
             <div className="zoo__list__animal__stats">
-                <span><i>Born date:</i> {animal.born.slice(0, 10) }</span>
+                <span><i>Born date:</i> {new Date(animal.born).getFullYear()}</span>
                 <span><i>Weight:</i> {animal.weight} kg.</span>
             </div>
+            <button onClick={showEdit}>Edit</button>
         </div>
     )
 }
